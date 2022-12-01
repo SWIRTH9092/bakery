083cba6f79a0f79dbff9b219e25c80455313190f
Your Name <you@example.com>
2022-12-01 08:12:00 -0500

    install dotenv, use port from env file

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
+require("dotenv").config();
 const app = express();
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3002;
 
 app.get('/', (req, res) => {
     res.send(`I am listening on port: ${PORT}`)
