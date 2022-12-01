da2a38de2813a5b0aa896296502b2464ba79098b
Your Name <you@example.com>
2022-12-01 07:56:10 -0500

    add port listening

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-PORT = process.env.PORT || 3001
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001
+
+app.get('/', (req, res) => {
+    res.send(`I am listening on port: ${PORT}`)
+})
+
+app.listen(PORT, () => {
+    console.log(`🧏‍♀️ on port: ${PORT}`)
+}) 
