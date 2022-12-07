0b4d245132a815849c499110cf0b9f5b582c54d3
Your Name <you@example.com>
2022-12-06 21:00:04 -0500

    add employee signin

---
+++ models/employee.js
@@ -0,0 +1,27 @@
+//-------------------------------------------
+// Import Dependencies
+//-------------------------------------------
+const mongoose = require("./connection")
+
+//-------------------------------------------
+// Define Model
+//-------------------------------------------
+// pull schema and model from mongoose
+const { Schema, model } = mongoose;
+
+// make user schema
+const employeeSchema = new Schema({
+    username: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    employeeId: {type: String, required: true},
+    firstName: {type: String, required: true },
+    lastName: {type: String, required: true}
+});
+
+// make User mdoel
+const User = model("Employee", employeeSchema);
+
+//-------------------------------------------
+// Export Model
+//-------------------------------------------
+module.exports = Employee
