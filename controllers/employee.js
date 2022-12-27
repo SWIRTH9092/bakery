1aff87e92bfbb10037cdececc52ba405e4da9101
Your Name <you@example.com>
2022-12-27 18:39:06 -0500

    added error screens for signup and login

--- controllers/employee.js
+++ controllers/employee.js
@@ -4,6 +4,7 @@
 const express = require("express")  // bring in express to make our app work
 const Employee = require("../models/employee");
 const bcrypt = require("bcryptjs");
+const { render } = require("ejs");
 
 //-------------------------------------------
 // Create Route
@@ -18,25 +19,67 @@ router.get("/", (req, res) => {
     res.render("employee/home.ejs")
 });
 
+//-------------------------------------------
+// For initial signup display screens
+//-------------------------------------------
 //  The Signup Routes (Get => form, post => submit form)
 router.get("/signup", (req, res) => {
     res.render("employee/signup.ejs")
 });
 
-
+//
 router.post("/signup", async (req, res) => {
     //encrypt password
     req.body.password = await bcrypt.hash(req.body.password, await bcrypt.genSalt(10))
     Employee.create(req.body, (err, employee) => {
-        //redirect to login page
+    if (!employee) {
+        console.log("not employee")
+        if (err.code === 11000) {
+            res.redirect("/employee/signup2")
+        } else {
+            res.send("fatal signup error.  Try again.")
+        }
+    } else {
+        //  redirect to login page
+        console.log("display employee")
         res.redirect("/employee/login")
+    }
     })
 });
 
+//-------------------------------------------
+// For duplicate Userid Error Screens
+//-------------------------------------------
+//  The Signup Routes (Get => form, post => submit form)
+router.get("/signup2", (req, res) => {
+    res.render("employee/signup2.ejs", {  signinmsg: "Username already used.  Please choose another." })
+});
+
+//
+router.post("/signup2", async (req, res) => {
+    //encrypt password
+    req.body.password = await bcrypt.hash(req.body.password, await bcrypt.genSalt(10))
+    Employee.create(req.body, (err, employee) => {
+    if (!employee) {
+        if (err.code === 11000) {
+            res.redirect("/employee/signup2")
+        } else {
+            res.send("fatal signup error.  Try again.")
+        }
+    } else {
+        //  redirect to login page
+        res.redirect("/employee/login")
+    }
+    })
+});
+
+//-------------------------------------------
+// Intial Login Routes
+//-------------------------------------------
 // The login Routes (Get => form, post => submit form)
 router.get("/login", (req, res) => {
     res.render("employee/login.ejs")
-});
+})
 
 router.post("/login", (req, res) => {
     //get the data from the request body
@@ -44,6 +87,36 @@ router.post("/login", (req, res) => {
     Employee.findOne ({ username }, (err, employee) => {
         //checking if user exists
         if (!employee) {
+            res.redirect("/employee/login2")
+        } else {
+            //check to see if password matches
+            const result = bcrypt.compareSync(password, employee.password);
+                if (result) {
+                    req.session.username = username;
+                    req.session.loggedIn = true;
+                    res.redirect("/menuupdt")
+                } else {
+                    res.redirect("/employee/login2")
+                }
+            }
+    })
+});
+
+//-------------------------------------------
+// Error Login Screen - wrong id or password
+//-------------------------------------------
+// The login Routes (Get => form, post => submit form)
+router.get("/login2", (req, res) => {
+    res.render("employee/login2.ejs", { loginmsg: "Username or Password Error" })
+});
+
+router.post("/login2", (req, res) => {
+    //get the data from the request body
+    const { username, password } = req.body;
+    Employee.findOne ({ username }, (err, employee) => {
+        //checking if user exists
+        if (!employee) {
+            console.log(err, err.code)
             res.send("employee doesn't exist");
         } else {
             //check to see if password matches
@@ -58,7 +131,6 @@ router.post("/login", (req, res) => {
             }
     })
 });
-
 //-------------------------------------------
 // Add logout to destory the session
 //-------------------------------------------
